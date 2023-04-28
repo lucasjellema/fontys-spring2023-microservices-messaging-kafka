03eace27b71ff835b44d3aa4fc822fb712bc21df
Lucas Jellema <lucasjellema@gmail.com>
2023-04-28 16:42:07 +0000

    add simple consumer and producer

---
+++ lab5-python-and-kafka/producer.py
@@ -0,0 +1,47 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+producer = KafkaProducer(bootstrap_servers=['localhost:29092', 'localhost:29093', 'localhost:29094'])
+
+# Asynchronous by default
+future = producer.send('test-topic', b'raw_bytes')
+
+# Block for 'synchronous' sends
+try:
+    record_metadata = future.get(timeout=10)
+except KafkaError:
+    # Decide what to do if produce request failed...
+    log.exception()
+    pass
+
+# Successful result returns assigned partition and offset
+print (record_metadata.topic)
+print (record_metadata.partition)
+print (record_metadata.offset)
+
+
+# produce keyed messages to enable hashed partitioning
+producer.send('test-topic', key=b'foo', value=b'bar')
+print ("Done one nore")
+
+# produce asynchronously
+for _ in range(5):
+    producer.send('test-topic', b'msg')
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    log.error('I am an errback', exc_info=excp)
+    # handle exception
+
+# produce asynchronously with callbacks
+producer.send('test-topic', b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
+
+# block until all async messages are sent
+producer.flush()
+
+# configure multiple retries
+producer = KafkaProducer(retries=5)
