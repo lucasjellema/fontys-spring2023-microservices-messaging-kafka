e9eb403d4b1b8baad715ff680205105983551536
Lucas Jellema <lucasjellema@gmail.com>
2023-05-05 07:17:19 +0200

    complete dotnet kafka client

---
+++ lab8-dotnet-and-kafka/consumer/consumer.cs
@@ -0,0 +1,43 @@
+using Confluent.Kafka;
+using System;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+class Consumer {
+
+    static void Main(string[] args)
+    {
+         var config = new ConsumerConfig {
+                            BootstrapServers = "localhost:29092,localhost:29093,localhost:29094", 
+                            ClientId = "dotnet-consumer",
+                            };
+            config.GroupId = "dotnet-kafka-consumer";
+            config.AutoOffsetReset = AutoOffsetReset.Earliest;
+            config.EnableAutoCommit = true;
+        const string topic = "test-topic";
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) => {
+            e.Cancel = true; // prevent the process from terminating.
+            cts.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<string, string>(
+            config).Build())
+        {
+            consumer.Subscribe(topic);
+            try {
+                while (true) {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed event from topic {topic} with key {cr.Message.Key,-10} and value {cr.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException) {
+                // Ctrl-C was pressed.
+            }
+            finally{
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ lab8-dotnet-and-kafka/producer/producer.cs
@@ -0,0 +1,46 @@
+using Confluent.Kafka;
+using System;
+using Microsoft.Extensions.Configuration;
+
+class Producer {
+    static void Main(string[] args)
+    {
+         var config = new ProducerConfig {
+                            BootstrapServers = "localhost:29092,localhost:29093,localhost:29094", 
+                            ClientId = "dotnet-producer",
+                            };
+
+               const string topic = "test-topic";
+
+        string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
+        string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+
+        using (var producer = new ProducerBuilder<string, string>(
+            config).Build())
+        {
+            var numProduced = 0;
+            Random rnd = new Random();
+            const int numMessages = 10;
+            for (int i = 0; i < numMessages; ++i)
+            {
+                var user = users[rnd.Next(users.Length)];
+                var item = items[rnd.Next(items.Length)];
+
+                producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
+                    (deliveryReport) =>
+                    {
+                        if (deliveryReport.Error.Code != ErrorCode.NoError) {
+                            Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                        }
+                        else {
+                            Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
+                            numProduced += 1;
+                        }
+                    });
+            }
+
+            producer.Flush(TimeSpan.FromSeconds(10));
+            Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+        }
+    }
+}
